e2ab40bdbfc21bd539e8f3c1eddc14506f4f3b00
Toh Wen Xiang <tohwenxiang@gmail.com>
2019-05-31 14:16:30 +0800

    31/5/19 that's it for today.

---
+++ Assets/Scripts/DungeonCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonCam : MonoBehaviour
+{
+    Camera theCamera;
+
+    public float pixelPerUnitScale;
+    public float pixelPerUnit;
+
+    private void Awake()
+    {
+        theCamera = GetComponent<Camera>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        UpdateOrthographicSize();
+    }
+
+    void UpdateOrthographicSize()
+    {
+        theCamera.orthographicSize = ((Screen.currentResolution.height) / (pixelPerUnitScale * pixelPerUnit)) * 0.5f;
+    }
+}
---
+++ Assets/Scripts/HighlightCursor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightCursor : MonoBehaviour
+{
+    Camera theCamera;
+
+    Ray newRay;
+
+    private void Awake()
+    {
+        theCamera = GetComponent<Camera>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetLookAtPosition();
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    void GetLookAtPosition()
+    {
+        //create a ray
+        newRay = theCamera.ScreenPointToRay(Input.mousePosition);
+        //create a plane
+        Plane targetPlane = new Plane(Vector3.back, Vector3.zero);
+        //create a variable to store ray distance
+        float rayDistance;
+
+        //ray cast using the plane
+        if(targetPlane.Raycast(newRay, out rayDistance))
+        {
+            //get the intersection point of the ray against the plane
+            Vector3 hitPosition = newRay.GetPoint(rayDistance);
+            Debug.DrawLine(newRay.origin, hitPosition, Color.red);
+        }
+        
+    }
+}
